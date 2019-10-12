df1a1692d4dd85b071ded3db32aa10bd35b4fbbc
ChulJJA <fpsemfaos@gmail.com>
2019-10-12 17:51:41 +0900

    Physics kk

--- DoubleCheck/Engine/Physics.h
+++ DoubleCheck/Engine/Physics.h
@@ -10,10 +10,12 @@ class Physics : public Component
 public:
     void Init(Object *obj) override;
     void Acceleration();
+    void Acceleration2();
     bool BoxToBoxCollision(Mesh mesh) const;
     bool CircleToCircleCollision(Object* object) const;
     void KnockBack(Object* object);
     void BasicMovement();
+    void BasicMovement2();
     void Update(float dt) override;
 
     void SetAcceleration(vector2 accel)
